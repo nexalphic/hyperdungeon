bd19dd20fc379749b0c5e0ab71f268491ce6af9e
nexalphic <nathangarces1@gmail.com>
2017-04-16 20:27:47 -0700

    Actually started on the generateRoom function.

--- hyperdungeon/generator.py
+++ hyperdungeon/generator.py
@@ -1,3 +1,22 @@
 import random
 
-pass
+doortypes = {
+    0: "worn",
+    50: "good",
+    70: "strong",
+    90: "fortified",
+    "last": "vault",
+}
+
+def generateDoor():
+    door = []
+    randDoortype = random.randomrand(0, 100)
+    for key  in doortypes
+        if ranDoortype <= doortypes[key]:
+            door.append(doortypes(key))
+            break
+
+def generateRoom(level):
+    for i in random.randrange(0, 4):
+        pass
+
